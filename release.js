45d2385b065503e7bd78531d65bd572a3e86389d
Sam Holmes <samholmes1337@gmail.com>
2015-05-26 10:48:35 +0100

    Updated manual 'usage' to reflect name change

--- release.js
+++ release.js
@@ -395,7 +395,7 @@ release = {
 
             // Print static data
             console.log("");
-            console.log("Usage: release [type]");
+            console.log("Usage: nwjs-release [type]");
             console.log("    where type 'patch' will increment to -.-.x");
             console.log("    where type 'minor' will increment to -.x.0");
             console.log("    where type 'major' will increment to x.0.0");
