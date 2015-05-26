311e6f1cd5b8973ad5b223c6c7b73fccd8f340b7
Sam Holmes <samholmes1337@gmail.com>
2015-05-26 23:16:38 +0100

    Added non-functional wizard for selecting version

--- release.js
+++ release.js
@@ -11,6 +11,37 @@ var release = {
         // Start version check
         release.version.read();
 
+    },
+
+    // Set user configuration / params
+    params : function(version) {
+
+        // Get different version amounts from title
+        var major = parseInt(version.split(".")[0]);
+        var minor = parseInt(version.split(".")[1]);
+        var patch = parseInt(version.split(".")[2]);
+
+        // Generate new values based on current version
+        var newMajor = (major + 1) + ".0.0";
+        var newMinor = major + "." + (minor + 1) + ".0";
+        var newPatch = major + "." + minor + "." + (patch + 1);
+
+        // Prompt the user for which version they would like to bump to
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "version",
+                message: "Which version do you want to release?",
+                default: newPatch,
+                choices: [
+                    {name: newMajor + " (Increment major version)", value: newMajor},
+                    {name: newMinor + " (Increment minor version)", value: newMinor},
+                    {name: newPatch + " (Increment patch version)", value: newPatch},
+                    new inquirer.Separator(),
+                    {name: "Exit (Don't release a new version)", value: "exit"}
+                ]
+            }
+        ]);
 
     },
 
@@ -39,7 +70,7 @@ var release = {
 
                     // If so, ask user what they'd like to do
                     var version = JSON.parse(data).version;
-                    release.version.bump(version);
+                    release.params(version);
 
                 } else {
 
@@ -58,7 +89,7 @@ var release = {
         // Bump the current version
         bump : function(version){
 
-            console.log(version);
+
 
         }
 
